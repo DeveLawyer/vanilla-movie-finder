52ec4dbabb7184b5b2ee39942041066966471376
Josue Narro <josue.narro.a@gmail.com>
2020-09-28 12:58:41 -0500

    Display images into main

--- script.js
+++ script.js
@@ -7,7 +7,21 @@ const main = document.querySelector('main');
 async function getMovie() {
   const resp = await fetch(POP_URL);
   const respData = await resp.json();
-  console.log(respData);
+
+  respData.results.forEach(movie => {
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.innerHTML = `<img
+                             src="${IMG_PATH}${movie.poster_path}"
+                             alt="${movie.title}"
+                           />
+                           <div class="movie-info">
+                             <h3>${movie.title}</h3>
+                             <span>${movie.vote_average}</span>
+                           </div>
+                           `;
+    main.append(movieEl);
+  });
 }
 
 getMovie();
